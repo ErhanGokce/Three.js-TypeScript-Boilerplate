4624e3c1cda67deb77fea21edf05d254ac6e8e20
ErhanGokce <erhangokceofis@gmail.com>
2024-06-09 22:40:08 +0300

    I added a rotation animation for this cube.

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,61 @@
-import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 1000)
-camera.position.z = 1.5
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 2
 
-const renderer = new THREE.WebGLRenderer({ antialias: true }) //true means turn on the 'antialias: upgrade graphics pixels to
-
-//creates a full screen js 
-//const canvas = document.getElementById('canvas') as HTMLCanvasElement
-//const renderer = new THREE.WebGLRenderer({ canvas: canvas}) 
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
-renderer.setSize(200, 200)
 
-/* window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-}) */
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', function () {
+  renderer.render(scene, camera)
+  
+})
 
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshNormalMaterial({ wireframe: true })
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+})
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-function animate() {
-  requestAnimationFrame(animate)
-
-  renderer.render(scene, camera)
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
 }
 
-animate()
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const clock = new THREE.Clock()
+let delta
+
+
+ function animate() {
+  requestAnimationFrame(animate)
+
+   delta = clock.getDelta()
+
+  cube.rotation.x += delta
+  cube.rotation.y += delta
+
+  renderer.render(scene, camera)
+  stats.update()
+} 
+
+
+  
+
+ animate()  
+
